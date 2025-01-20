b542f2658f2d3e9608ca48259fe0535fca2e6f29
Pritipaudel <preetipaudel456@gmail.com>
2025-01-20 19:15:30 +0545

    second-commit

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    # id = models.AutoField() django default
+    name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null = True , blank=True)
+    image = models.ImageField()
+    file = models.FileField()
+
+class Marks(models.Model):
+    pass
